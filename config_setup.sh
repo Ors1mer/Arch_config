2e5b2333df1a3bb830e70dbe88a207b410798dda
Ors1mer <ors1mer_dev [[at]] proton.me>
2023-02-21 16:36:05 +0200

    fix typo

--- config_setup.sh
+++ config_setup.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
-# Setups all of the configs into the system
+# copies configs to their place
 
 echo "Copying BSPWM & SXHKD configs..." 
 # bspwm
-[ ! -d $HOME/.config/bspwm] && mkdir $HOME/.config/bspwm
+[ ! -d $HOME/.config/bspwm ] && mkdir $HOME/.config/bspwm
 cp -r $PWD/configs/bspwmrc $HOME/.config/bspwm/bspwmrc
 # sxhkd
-[ ! -d $HOME/.config/sxhkd] && mkdir $HOME/.config/sxhkd
+[ ! -d $HOME/.config/sxhkd ] && mkdir $HOME/.config/sxhkd
 cp -r $PWD/configs/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
 
 # polybar
 echo "Copying Polybar config..."
-[ ! -d $HOME/.config/polybar] && mkdir $HOME/.config/polybar
+[ ! -d $HOME/.config/polybar ] && mkdir $HOME/.config/polybar
 cp -r $PWD/configs/polybar/* $HOME/.config/polybar/*
 
 # alacritty
 echo "Copying Alacritty config..."
-[ ! -d $HOME/.config/alacritty] && mkdir $HOME/.config/alacritty
+[ ! -d $HOME/.config/alacritty ] && mkdir $HOME/.config/alacritty
 cp -r $PWD/configs/alacritty/* $HOME/.config/alacritty/*
 
 # vim
 echo "Copying vim setup and addons..."
-[ ! -d $HOME/.vim] && mkdir $HOME/.vim
+[ ! -d $HOME/.vim ] && mkdir $HOME/.vim
 cp -r $PWD/configs/.vim/* $HOME/.vim/*
 cp $PWD/configs/.vimrc $HOME/.vimrc
 
 # pywal and wallpaper
 echo "Setting up wallpaper & terminal colorscheme..."
-[ ! -d $HOME/pictures/wallpapers] && mkdir $HOME/pictures/wallpapers
+[ ! -d $HOME/pictures/wallpapers ] && mkdir $HOME/pictures/wallpapers
 cp -r $PWD/images/* $HOME/pictures/wallpapers
 #cat ~/.cache/wal/sequences &
 
 bspc wm -r
 wal -i $HOME/pictures/wallpapers/cp3.jpg
+
